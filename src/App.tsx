dcdc133b990019e6b66c60002b4b6700fb6b7276
gasparotto <matheus.andradeg@outlook.com>
2021-12-08 22:57:45 -0300

    routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import * as React from 'react'
 import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react'
 
 import { ColorModeSwitcher } from './ColorModeSwitcher'
+import AppRoutes from './pages'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
+      <Grid minH="100vh" paddingX={10}>
         <ColorModeSwitcher justifySelf="flex-end" />
+        <AppRoutes />
       </Grid>
     </Box>
   </ChakraProvider>
