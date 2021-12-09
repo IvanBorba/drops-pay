e8fc81ab11608590352133a5702cdaafd8999fdf
gasparotto <matheus.andradeg@outlook.com>
2021-12-08 23:06:04 -0300

    grid setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react'
+import { ChakraProvider, Box, Grid, theme, GridItem } from '@chakra-ui/react'
 
 import { ColorModeSwitcher } from './ColorModeSwitcher'
 import AppRoutes from './pages'
@@ -8,9 +8,12 @@ import AppRoutes from './pages'
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" paddingX={10}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <AppRoutes />
+      <Grid minH="100vh" pr={10} templateColumns={'minmax(10vw, 250px) auto '}>
+        <GridItem rowSpan={1} colSpan={1} bg="tomato"></GridItem>
+        <Grid rowSpan={1} colSpan={1} templateRows={'minmax(5vh, 48px) auto'}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <AppRoutes />
+        </Grid>
       </Grid>
     </Box>
   </ChakraProvider>
